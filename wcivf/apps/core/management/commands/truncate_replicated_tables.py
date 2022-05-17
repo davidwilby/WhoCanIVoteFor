3e7978c593e25536a4124925afc4b93d4c4082fd
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-05-17 16:55:53 +0100

    Add management command to truncate tables

---
+++ wcivf/apps/core/management/commands/truncate_replicated_tables.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+
+from django.db import connection
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                BEGIN;
+                TRUNCATE "auth_permission", "auth_group_permissions", "auth_user_user_permissions", "django_admin_log", "robots_rule_sites", "django_content_type", "django_site", "django_migrations", "spatial_ref_sys"  RESTART IDENTITY;
+                COMMIT;
+                """
+            )
