e6c892237976e0aca3583c8b32d23801a9005d99
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-05-18 09:56:36 +0100

    Migrate default database in migrate_logger.sh
    Migrating the logger database before migrating the default database
    can lead to errors. So even though we drop the database and recreate
    with empty tables later, still run the migrate command for the default
    DB before we migrate the logger DB.

--- deployscripts/migrate_logger.sh
+++ deployscripts/migrate_logger.sh
@@ -2,4 +2,6 @@
 set -xeE
 
 source /var/www/wcivf/env/bin/activate
+# "default" database has to be migrated first to avoid errors migrating logger
+python /var/www/wcivf/code/manage.py migrate --noinput
 python /var/www/wcivf/code/manage.py migrate --database=logger --noinput
