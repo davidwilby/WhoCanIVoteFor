42b70ee08d9c299d9f11750084e3bbd7b88a73c1
Virginia Dooley <vcdooley@gmail.com>
2024-04-02 14:39:51 +0100

    Reformatting
    Update casing in test to match response

--- wcivf/apps/people/migrations/0047_person_statement_to_voters_last_updated.py
+++ wcivf/apps/people/migrations/0047_person_statement_to_voters_last_updated.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("people", "0046_personpost_deselected_personpost_deselected_source"),
     ]
--- wcivf/apps/people/migrations/0048_person_blue_sky_url_person_other_url_and_more.py
+++ wcivf/apps/people/migrations/0048_person_blue_sky_url_person_other_url_and_more.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("people", "0047_person_statement_to_voters_last_updated"),
     ]
