cdd1bccffa480ca5d766481ee6235f787efdc6d0
Virginia Dooley <vcdooley@gmail.com>
2024-04-10 14:37:45 +0100

    Don't show upcoming elections on home page

--- wcivf/apps/core/views.py
+++ wcivf/apps/core/views.py
@@ -1,13 +1,13 @@
 import datetime
 import os
 
-from core.helpers import may_election_day_this_year
+
 from django import http
 from django.conf import settings
 from django.urls import reverse
 from django.utils import timezone, translation
 from django.views.generic import FormView, TemplateView, View
-from elections.models import PostElection
+
 
 from .forms import PostcodeLookupForm
 
@@ -18,9 +18,7 @@ class TranslatedTemplateView(TemplateView):
         base_template_name, ext = self.template_name.rsplit(".", 1)
         current_language = translation.get_language()
         if current_language != "en":
-            templates.insert(
-                0, f"{base_template_name}_{current_language}.{ext}"
-            )
+            templates.insert(0, f"{base_template_name}_{current_language}.{ext}")
         return templates
 
 
@@ -28,10 +26,7 @@ class PostcodeFormView(FormView):
     form_class = PostcodeLookupForm
 
     def get(self, request, *args, **kwargs):
-        if (
-            request.GET.get("postcode")
-            and "invalid_postcode" not in self.request.GET
-        ):
+        if request.GET.get("postcode") and "invalid_postcode" not in self.request.GET:
             redirect_url = reverse(
                 "postcode_view", kwargs={"postcode": request.GET["postcode"]}
             )
@@ -57,9 +52,7 @@ class PostcodeFormView(FormView):
 
     def form_valid(self, form):
         postcode = form.cleaned_data["postcode"]
-        self.success_url = reverse(
-            "postcode_view", kwargs={"postcode": postcode}
-        )
+        self.success_url = reverse("postcode_view", kwargs={"postcode": postcode})
         return super().form_valid(form)
 
 
@@ -69,21 +62,24 @@ class HomePageView(PostcodeFormView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        today = datetime.datetime.today()
-        delta = datetime.timedelta(weeks=4)
-        cut_off_date = today + delta
+        # Comment in this code to hide upcoming elections on the homepage
+        context["upcoming_elections"] = None
 
-        context["upcoming_elections"] = (
-            PostElection.objects.filter(
-                election__election_date__gte=today,
-                election__election_date__lte=cut_off_date,
-                # Temporarily removed following May elections #
-                election__any_non_by_elections=False,
-            )
-            .exclude(election__election_date=may_election_day_this_year())
-            .select_related("election", "post")
-            .order_by("election__election_date")
-        )
+        # Comment in this code to show upcoming elections on the homepage
+        # today = datetime.datetime.today()
+        # delta = datetime.timedelta(weeks=4)
+        # cut_off_date = today + delta
+        # context["upcoming_elections"] = (
+        #     PostElection.objects.filter(
+        #         election__election_date__gte=today,
+        #         election__election_date__lte=cut_off_date,
+        #         # Temporarily removed following May elections #
+        #         election__any_non_by_elections=False,
+        #     )
+        #     .exclude(election__election_date=may_election_day_this_year())
+        #     .select_related("election", "post")
+        #     .order_by("election__election_date")
+        # )
         polls_open = timezone.make_aware(
             datetime.datetime.strptime("2019-12-12 7", "%Y-%m-%d %H")
         )
@@ -99,9 +95,7 @@ class HomePageView(PostcodeFormView):
         context["show_gb_id_messaging"] = getattr(
             settings, "SHOW_GB_ID_MESSAGING", False
         )
-        context["show_results_chart"] = getattr(
-            settings, "SHOW_RESULTS_CHART", False
-        )
+        context["show_results_chart"] = getattr(settings, "SHOW_RESULTS_CHART", False)
 
         return context
 
@@ -121,9 +115,7 @@ class StatusCheckView(View):
     @property
     def server_is_dirty(self):
         if getattr(settings, "CHECK_HOST_DIRTY", False):
-            dirty_file_path = os.path.expanduser(
-                getattr(settings, "DIRTY_FILE_PATH")
-            )
+            dirty_file_path = os.path.expanduser(getattr(settings, "DIRTY_FILE_PATH"))
 
             if os.path.exists(dirty_file_path):
                 return True
