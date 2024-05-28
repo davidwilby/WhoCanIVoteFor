43953ef4ebf59135fdefe307411b2c72e1669870
Virginia Dooley <vcdooley@gmail.com>
2024-05-28 17:55:33 +0100

    Create GeneralElectionParty model

--- wcivf/apps/parties/models.py
+++ wcivf/apps/parties/models.py
@@ -263,6 +263,39 @@ class PartyEmblem(TimeStampedModel):
         ordering = ("-default", "-active", "ec_emblem_id")
 
 
+class GeneralElectionParty(models.Model):
+    parent = models.ForeignKey(
+        Party, related_name="general_election_parties", on_delete=models.CASCADE
+    )
+    post_election = models.ForeignKey(
+        "elections.PostElection", on_delete=models.CASCADE
+    )
+    name = models.CharField(blank=True, max_length=100)
+    facebook_page = models.URLField(blank=True, max_length=800)
+    instagram = models.URLField(blank=True, max_length=100)
+    homepage = models.URLField(blank=True, max_length=800)
+    email = models.EmailField(blank=True)
+    youtube_profile_url = models.URLField(blank=True, max_length=800)
+    contact_page_url = models.URLField(blank=True, max_length=800)
+    file_url = models.URLField(
+        blank=True,
+        max_length=800,
+        help_text="The url to the google sheet the object was imported from. Not to be displayed.",
+    )
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def label(self):
+        """
+        Return a string with 'the' added if the name ends with 'Party'
+        """
+        if self.name.lower().endswith("party"):
+            return f"the {self.name}"
+        return self.name
+
+
 class LocalParty(TimeStampedModel):
     parent = models.ForeignKey(
         Party, related_name="local_parties", on_delete=models.CASCADE
